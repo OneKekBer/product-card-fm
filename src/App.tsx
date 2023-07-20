a9b9d53ea455a410694a97fbcc83125f6c44d179
OneKek <iliaberezovsky227@gmai.com>
2023-07-20 13:34:51 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import img1 from "./assets/img1.jpg";
 import cart from "./assets/icon-cart.svg";
